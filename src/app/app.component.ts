c7886af75c4358d8c3463d42fa88b5da569f6487
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2024-08-14 23:21:02 +0330

    :sparkles: composing with components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoList } from './components/todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodoList],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'erster_einblick_auf_angular';
---
+++ src/app/components/todo-item/todo-list-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'todo-list-item',
+  templateUrl: './todo-list-item.component.html',
+  styleUrl: './todo-list-item.component.css',
+})
+export class TodoListItem {}
