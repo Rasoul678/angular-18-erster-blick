d18c5a2419b8e427a2c8fcc3e6954fb93f7e1364
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2024-08-26 00:26:52 +0330

    :sparkles: define routes and routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TodoList } from './components/todo-list/todo-list.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodoList],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppBanner } from './components/app-banner/app-banner.component';
+import { OptimizeImageComponent } from './components/optimize-image/optimize-image.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'App Home Page',
+    component: AppBanner,
+  },
+  {
+    path: 'user',
+    title: 'App User Page',
+    component: OptimizeImageComponent,
+  },
+];
