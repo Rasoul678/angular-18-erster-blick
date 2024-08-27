c026152694c8d25a757a576494ef632ad285ba02
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2024-08-27 20:24:59 +0330

    :sparkles: Formatting data with pipes

--- src/app/components/pipes/pipes.component.ts
+++ src/app/components/pipes/pipes.component.ts
@@ -1,14 +1,24 @@
-import { LowerCasePipe, UpperCasePipe } from '@angular/common';
+import {
+  CurrencyPipe,
+  DatePipe,
+  DecimalPipe,
+  LowerCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipes',
   standalone: true,
-  imports: [UpperCasePipe, LowerCasePipe],
+  imports: [UpperCasePipe, LowerCasePipe, DatePipe, DecimalPipe, CurrencyPipe],
   templateUrl: './pipes.component.html',
   styleUrl: './pipes.component.css',
 })
 export class PipesComponent {
   loudMessage = 'we think you are doing great!';
   username = 'RASOUL';
+
+  birthday = new Date(1988, 3, 7);
+  number = 1234.6788;
+  cost = 4560.34;
 }
